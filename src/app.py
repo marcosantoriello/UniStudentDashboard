01bba21ae3f2eca77095566d30a137278f903c24
Marco Santoriello <m.santoriello37@studenti.unisa.it>
2024-07-10 01:33:15 +0200

    Added connection to database and table creation script

--- src/app.py
+++ src/app.py
@@ -1,26 +1,45 @@
 import sqlite3
 import os
-from flask import flask, g, render_template, request, redirect, Flask
+import logging
+from flask import Flask, g, render_template, request, redirect, Flask, flash
 
 # TO CHANGE!!
-DATABASE = '/data/exams.db'
+DATABASE = './data/exams.sqlite'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
 def connect_db():
+    print('Connecting to database...', flush=True)
     """app.config["DATABASE"] access the flask app configuration value DATABASE"""
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
+    with open('./data/schema.sql') as f:
+        conn.executescript(f.read())
+    print('Table created!', flush=True)
     return conn
 
 
 def get_db():
-    """If it doesn't exists, it opens an new db connection"""
+    """If it doesn't exists, it opens a new db connection"""
+    print('Opening database...', flush=True)
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
+
+
+def create_app():
+    db = get_db()
+    cursor = db.cursor()
+
+
+
+def create_app():
+    with app.app_context():
+        init_db()
+    return app
+
 
 @app.teardown_appcontext
 def close_db(error):
@@ -28,7 +47,17 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+
 @app.route('/')
 def index():
     db = get_db()
-    
+    # just for testing purposes
+    db.execute('INSERT into exam (title, cfu) values (?, ?)',
+               ["Analisi Matematica", 6])
+    res = db.execute('SELECT * FROM exam').fetchall()
+
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
